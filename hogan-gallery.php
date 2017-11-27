c51ed2cddd30cf52e811af6b5c01de771a3bc9f4
Stian Hassel Øveråsen <stian@dekode.no>
2017-11-27 10:44:45 +0100

    Bump to version 1.0.0

--- hogan-gallery.php
+++ hogan-gallery.php
@@ -3,7 +3,7 @@
  * Plugin Name: Hogan Module: Gallery
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-gallery
  * Description: Image Gallery Module for Hogan
- * Version: 1.0.0-dev
+ * Version: 1.0.0
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
