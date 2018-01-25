c4a5e2a0eaad35427676fabb98bb336a67c085ad
pederan <peder1976@gmail.com>
2018-01-25 14:13:10 +0100

    added more itemprops

--- assets/template.php
+++ assets/template.php
@@ -28,11 +28,11 @@ if ( ! empty( $this->heading ) ) {
 <div class="items" itemscope itemtype="http://schema.org/ImageGallery">
 	<?php foreach ( $this->items as $item ) : ?>
 		<figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-			<a href="<?php echo esc_url( $item['url'] ); ?>" data-size="<?php echo esc_attr( $item['width'] . 'x' . $item['height'] ); ?>">
-				<img src="<?php echo esc_attr( $item['sizes'][ apply_filters( 'hogan/module/gallery/image_size', 'thumbnail' ) ] ); ?>" alt="<?php echo esc_attr( $item['alt'] ); ?>"/>
+			<a href="<?php echo esc_url( $item['url'] ); ?>" itemprop="contentUrl" data-size="<?php echo esc_attr( $item['width'] . 'x' . $item['height'] ); ?>">
+				<img src="<?php echo esc_attr( $item['sizes'][ apply_filters( 'hogan/module/gallery/image_size', 'thumbnail' ) ] ); ?>" itemprop="thumbnail" alt="<?php echo esc_attr( $item['alt'] ); ?>"/>
 			</a>
 			<?php if ( ! empty( $item['caption'] ) ) : ?>
-				<figcaption><?php echo wp_kses_post( $item['caption'] ); ?></figcaption>
+				<figcaption itemprop="caption description"><?php echo wp_kses_post( $item['caption'] ); ?></figcaption>
 			<?php endif; ?>
 		</figure>
 	<?php endforeach; ?>
