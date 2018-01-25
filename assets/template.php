fe5ffb029b464ef1bdcc20585a2e9e67c4d5eaa4
pederan <peder1976@gmail.com>
2018-01-25 13:43:26 +0100

    Fixed html and updated version number and licence

--- assets/template.php
+++ assets/template.php
@@ -11,7 +11,7 @@
  * @package Hogan
  */
 
-declare( strict_types = 1 );
+declare( strict_types=1 );
 namespace Dekode\Hogan;
 
 if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Gallery ) ) {
@@ -24,20 +24,17 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Gallery ) ) {
 	<h2 class="heading"><?php echo esc_html( $this->heading ); ?></h2>
 <?php endif; ?>
 
-<ul class="items">
+<div class="items" itemscope itemtype="http://schema.org/ImageGallery">
 	<?php foreach ( $this->items as $item ) : ?>
-		<li class="item">
-			<figure>
-				<a href="<?php echo esc_url( $item['url'] ); ?>"
-				   data-size="<?php echo esc_attr( $item['width'] . 'x' . $item['height'] ); ?>">
-					<img src="<?php echo esc_attr( $item['sizes'][ apply_filters( 'hogan/module/gallery/image_size', 'thumbnail' ) ] ); ?>"
-						 alt="<?php echo esc_attr( $item['alt'] ); ?>" />
-				</a>
-
-				<?php if ( ! empty( $item['caption'] ) ) : ?>
-					<figcaption><?php echo wp_kses_post( $item['caption'] ); ?></figcaption>
-				<?php endif; ?>
-			</figure>
-		</li>
+		<figure class="item" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			<a href="<?php echo esc_url( $item['url'] ); ?>"
+			   data-size="<?php echo esc_attr( $item['width'] . 'x' . $item['height'] ); ?>">
+				<img src="<?php echo esc_attr( $item['sizes'][ apply_filters( 'hogan/module/gallery/image_size', 'thumbnail' ) ] ); ?>"
+					 alt="<?php echo esc_attr( $item['alt'] ); ?>"/>
+			</a>
+			<?php if ( ! empty( $item['caption'] ) ) : ?>
+				<figcaption><?php echo wp_kses_post( $item['caption'] ); ?></figcaption>
+			<?php endif; ?>
+		</figure>
 	<?php endforeach; ?>
-</ul>
+</div>
--- hogan-gallery.php
+++ hogan-gallery.php
@@ -4,10 +4,10 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/hogan-gallery
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/hogan-gallery
  * Description: Image Gallery Module for Hogan
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Dekode
  * Author URI: https://dekode.no
- * License: GPL-3.0
+ * License: GPL-3.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
  *
  * * Text Domain: hogan-gallery
