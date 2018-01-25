f9dd8252afe008b39bf5e62d5f0d9f900e2d0c0b
Petter Walbø Johnsgård <petter@dekode.no>
2018-01-25 12:25:16 +0100

    Fix linting errors

--- src/slider.js
+++ src/slider.js
@@ -1,10 +1,12 @@
 /**
  * Globals
  */
-const Flickity = window.Flickity;
-const hoganGallery = window.hoganGallery;
+const {
+	Flickity,
+	hoganGallery,
+} = window;
 
 /**
  * Hogan slider
  */
-new Flickity( '.hogan-gallery-carousel', hoganGallery.flickityConfig );
+new Flickity( '.hogan-gallery-carousel', hoganGallery.flickityConfig ); // eslint-disable-line no-new
