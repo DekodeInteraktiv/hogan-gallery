d0c12ba62a3e7f603ae6ffb8f6bf51473cf64615
Petter Walbø Johnsgård <petter@dekode.no>
2018-01-25 15:57:06 +0100

    Init all sliders on a page

--- src/slider.js
+++ src/slider.js
@@ -9,4 +9,8 @@ const {
 /**
  * Hogan slider
  */
-new Flickity( '.hogan-gallery-carousel', hoganGallery.flickityConfig ); // eslint-disable-line no-new
+const sliders = document.querySelectorAll( '.hogan-gallery-carousel' );
+
+Array.prototype.slice.call( sliders ).forEach( slider => {
+	new Flickity( slider, hoganGallery.flickityConfig ); // eslint-disable-line no-new
+} );
