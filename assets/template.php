f4bdd9e8fb899ae910a80ab243c7ae433e43de6d
pederan <peder1976@gmail.com>
2018-02-15 21:14:42 +0100

    Removed heading from list of available properties in template comments

--- assets/template.php
+++ assets/template.php
@@ -5,7 +5,6 @@
  * $this is an instace of the Gallery object.
  *
  * Available properties:
- * $this->heading (string) Module heading.
  * $this->items (array) Gallery items.
  *
  * @package Hogan
