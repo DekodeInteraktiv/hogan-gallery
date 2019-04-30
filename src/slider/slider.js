b1fd15315d9057a5200bbf295f7ebf5a67991bd5
Rasmus Rimestad <rasmus@dekode.no>
2019-04-30 10:30:09 +0200

    Changed so gallery is loaded after page is loaded

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -9,12 +9,15 @@ const {
 /**
  * Hogan slider
  */
-const sliders = document.querySelectorAll( '.hogan-gallery-carousel' );
+window.onload = () => {
+	const sliders = document.querySelectorAll('.hogan-gallery-carousel');
 
-Array.prototype.slice.call( sliders ).forEach( slider => {
-	new Flickity( slider, hoganGallery.flickityConfig ); // eslint-disable-line no-new
+	Array.prototype.slice.call(sliders).forEach(slider => {
+		new Flickity(slider, hoganGallery.flickityConfig); // eslint-disable-line no-new
 
-	if ( slider.hasAttribute( 'data-pswp-uid' ) ) {
-		new window.hogan.PhotoSwipe( slider, hoganGallery.photoswipeConfig ); // eslint-disable-line no-new
-	}
-} );
+		if (slider.hasAttribute('data-pswp-uid')) {
+			new window.hogan.PhotoSwipe(slider, hoganGallery.photoswipeConfig); // eslint-disable-line no-new
+		}
+	});
+}
+
